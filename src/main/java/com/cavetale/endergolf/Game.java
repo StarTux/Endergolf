521785ce4a9a53e7050a3c1ddef88525a6f7fb66
StarTux <ths.loy@gmail.com>
2025-11-29 22:33:20 +0100

    Disable pvp

--- src/main/java/com/cavetale/endergolf/Game.java
+++ src/main/java/com/cavetale/endergolf/Game.java
@@ -184,6 +184,7 @@ public final class Game {
         world.setGameRule(GameRule.FALL_DAMAGE, false);
         world.setGameRule(GameRule.DROWNING_DAMAGE, false);
         world.setGameRule(GameRule.LOCATOR_BAR, true);
+        world.setGameRule(GameRule.PVP, true);
         world.setViewDistance(32);
         if (world.getWorldBorder().getSize() < 1024.0) {
             world.getWorldBorder().setSize(1024.0);
